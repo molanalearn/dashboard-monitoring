f2b72739870f43708dc6ae94b4e3fcafad4ba877
ssd <ifanmolanalearn@gmail.com>
2023-08-18 20:31:27 +0700

    import Beautifulsoup4 dan menggunakan package requests

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -1,3 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 def ekstraksi_data():
     """
     Tanggal: 29 Juli 2023
@@ -9,19 +13,33 @@ def ekstraksi_data():
     Dirasakan: Dirasakan (Skala MMI): III Morotai Selatan, II-III Halmahera Timur, II Halmahera Utara
     :return:
     """
-    hasil = dict()
-    hasil['tanggal'] = '29 Juli 2023'
-    hasil['waktu'] = '18:19:11 WIB'
-    hasil['magnitudo'] = 5.0
-    hasil['kedalaman'] = '51 km'
-    hasil['geo'] = {'ls': 1.40,'bt': 128.31}
-    hasil['pusat'] = 'Pusat gempa berada di laut 21 km BaratLaut Halmahera Timur'
-    hasil['dirasakan'] = 'Dirasakan (Skala MMI): III Morotai Selatan, II-III Halmahera Timur, II Halmahera Utara'
+    try:
+        content = requests.get('https://www.bmkg.go.id/')
+    except Exception:
+        return None
 
-    return hasil
+    if content.status_code == 200:
+        print(content.text)
+        # soup = BeautifulSoup(content)
+        # print(soup.prettify())
+        hasil = dict()
+        hasil['tanggal'] = '29 Juli 2023'
+        hasil['waktu'] = '18:19:11 WIB'
+        hasil['magnitudo'] = 5.0
+        hasil['kedalaman'] = '51 km'
+        hasil['geo'] = {'ls': 1.40,'bt': 128.31}
+        hasil['pusat'] = 'Pusat gempa berada di laut 21 km BaratLaut Halmahera Timur'
+        hasil['dirasakan'] = 'Dirasakan (Skala MMI): III Morotai Selatan, II-III Halmahera Timur, II Halmahera Utara'
+        return hasil
+    else:
+        return None
 
 
 def tampilkan_data(result):
+    if result is None:
+        print('Tidak bisa menemukan data gempa terkini')
+        return
+
     print('Gempa terakhir berdasarkan BMKG')
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['waktu']}")
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 """
 Aplikasi deteksi gempa terkini
 MODULARISASI DENGAN FUNCTION
-MOCULARISASI DENGAN PACKAGE
+MODULARISASI DENGAN PACKAGE
 """
 import gempaterkini
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     print('Aplikasi Utama')
-    result = gempaterkini.ekstraksi_data ()
+    result = gempaterkini.ekstraksi_data()
     gempaterkini.tampilkan_data(result)
